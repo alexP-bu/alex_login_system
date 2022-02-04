cce2c65c95b844e438e06b8aad119298b4dd4f8e
alexP-bu <apozin@bu.edu>
2022-02-04 15:04:39 -0500

    working on server access of database

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-import express from 'express'
-import path from 'path'
-import mysql from 'mysql'
-import dbRoutes from './modules/dbRoutes.js';
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import userRouter from './modules/userRouter.js';
 
+dotenv.config({path: '../.env'});
 const app = express();
 const router = express.Router();
+
 const PORT = 8080;
 
 //database setup
 
-
+//routes
+app.use('/users', userRouter);
 app.use('/', router);
-app.use(express.static(path.resolve('./views/')));
+app.all('*', (req, res) => {
+   
+});
 
+//router
 router.get('/', (req, res) => {
+   app.use(express.static(path.resolve('./views/')));
    res.sendFile(path.resolve('./views/index.html'));
 });
 
