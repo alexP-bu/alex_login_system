82e59f2d361ec439e8118bf242df46694da1e9fe
alexP-bu <apozin@bu.edu>
2022-02-04 19:01:49 -0500

    worked on database init file, updated docker compose to init db properly

--- db/init.sql
+++ db/init.sql
@@ -1,31 +1,29 @@
-CREATE DATABASE IF NOT EXISTS serverdb;
-USE serverdb;
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS salts;
+CREATE DATABASE IF NOT EXISTS socialnetworkdb;
+USE socialnetworkdb;
 
 CREATE TABLE users(
-    id INTEGER NOT NULL,
-    username VARCHAR(255),
-    pwhash VARCHAR(255),
-    firstname VARCHAR(255),
-    email VARCHAR(255),
+    id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username   VARCHAR(254) DEFAULT NULL,
+    pwhash     VARCHAR(254) DEFAULT NULL,
+    firstname  VARCHAR(254) DEFAULT NULL,
+    email      VARCHAR(254) DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    CONSTRAINT UQ_Users_Email UNIQUE (email),
+    CONSTRAINT UQ_Users_Email UNIQUE (email)
 );
 
 CREATE TABLE salts(
-    id INTEGER NOT NULL,
-    userid INTEGER,
-    salt VARCHAR(255),
-    PRIMARY KEY(id),
+    id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username   VARCHAR(254),
+    salt       VARCHAR(254),
+    PRIMARY KEY (id),
+
 );
 
 CREATE TABLE tokens(
-    id INTEGER NOT NULL,
-    userid INT,
-    token VARCHAR(255),
+    id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    token VARCHAR(254),
     stamp DATETIME,
-    PRIMARY KEY(id),
+    PRIMARY KEY(id)
 );
