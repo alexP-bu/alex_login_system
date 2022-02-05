7cc77f60cf3e9adef32340ce3a1f1cd3e833f5bb
alexP-bu <apozin@bu.edu>
2022-02-04 19:28:50 -0500

    named docker image; fixed init.sql to have VARCHAR(255)

--- db/init.sql
+++ db/init.sql
@@ -4,10 +4,10 @@ USE socialnetworkdb;
 
 CREATE TABLE users(
     id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username   VARCHAR(254) DEFAULT NULL,
-    pwhash     VARCHAR(254) DEFAULT NULL,
-    firstname  VARCHAR(254) DEFAULT NULL,
-    email      VARCHAR(254) DEFAULT NULL,
+    username   VARCHAR(255) DEFAULT NULL,
+    pwhash     VARCHAR(255) DEFAULT NULL,
+    firstname  VARCHAR(255) DEFAULT NULL,
+    email      VARCHAR(255) DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     CONSTRAINT UQ_Users_Email UNIQUE (email)
@@ -15,14 +15,14 @@ CREATE TABLE users(
 
 CREATE TABLE salts(
     id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username   VARCHAR(254),
-    salt       VARCHAR(254),
+    username   VARCHAR(255),
+    salt       VARCHAR(255),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE tokens(
     id    BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    token VARCHAR(254),
+    token VARCHAR(255),
     stamp DATETIME,
     PRIMARY KEY(id)
 );
